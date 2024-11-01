98aba6e13289863302d3e8f2e494ce5306809033
Zainab <zhaseeba1@appdev.yearup.org>
2024-11-01 12:53:19 -0500

    work in progress

---
+++ Functions/substrings.py
@@ -0,0 +1,14 @@
+# Create a new script file named substrings.py.  
+# ∗ Start by working with a variable containing a full name, e.g. name = ‘John Doe’ 
+# ∗ Write code to find the space in the name, then extract the first and last name and 
+# display the results. Output might look like: 
+# Full Name: John Doe 
+# First Name: John 
+# Last Name: Doe 
+# 2. Once you have the code working for one name, test your script with at least three times 
+# with different names. The full output (full name, first name, last name) should be 
+# printed for each name. Examples: 
+# parse_and_display(‘John Doe’) 
+# parse_and_display(‘Grace Flores’) 
+# parse_and_display(‘JB Oinonen’)
+
